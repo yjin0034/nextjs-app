61d27be94c19485e413cae79380e048c5ea7b785
yjin0034 <yjin0034@naver.com>
2023-02-10 05:56:28 +0900

    05. 포스트 데이터를 가져와서 보여주기(remark)

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,49 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import React from 'react';
+import { getAllPostIds, getPostData } from "../../lib/post";
 
-const Post = () => {
+const Post = ({ postData }: {
+  postData: {
+    title: string
+    date: string
+    contentHtml : string
+  }
+}) => {
   return (
-    <div>[id]</div>
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>
+          {postData.date}
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
   )
 }
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [ { params: { id: 'pre-rendering' } }, { params: { id: 'ssg-ssr' } } ]
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  console.log('params', params);
+  // { id: 'ssg-ssr' }
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData
+    }
+  }
+}
