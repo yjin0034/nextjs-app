2de436583dc95a689f5dd9cae7d6dd02a1302002
yjin0034 <yjin0034@naver.com>
2023-02-10 04:24:35 +0900

    03. getStaticProps를 이용한 포스트 리스트 나열

--- lib/post.ts
+++ lib/post.ts
@@ -24,7 +24,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...(matterResult.data as { date: string; title: string })
+      ...matterResult.data as { date: string; title: string }
     }
   })
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import homeStyles from '../styles/Home.module.css'
+import type { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { getSortedPostsData } from '../lib/post';
+import homeStyles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const Home = ({allPostsData}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) => {
   return (
     <div>
       <Head>
@@ -18,7 +25,15 @@ const Home: NextPage = () => {
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
         <ul className={homeStyles.list}>
-
+          {allPostsData.map(({id, title, date}) => 
+          <li className={homeStyles.listItem} key={id}>
+            <a>{title}</a>  
+            <br />
+            <small className={homeStyles.lightText}>
+              {date}
+            </small>
+          </li>
+          )}
         </ul>
       </section>
     </div>
@@ -26,3 +41,12 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
