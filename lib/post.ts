31639851c3843b8f2c56e24b0e1fea43550af45c
yjin0034 <yjin0034@naver.com>
2023-02-10 04:10:28 +0900

    02. 마크다운 파일을 데이터로 추출하기

---
+++ lib/post.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  // /posts 파일 이름을 잡아주기
+  const fileNames = fs.readdirSync(postsDirectory);
+  // fileNames ['pre-rendering.md', 'ssg-ssr.md']
+
+  const allPostsData = fileNames.map(fileName => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...(matterResult.data as { date: string; title: string })
+    }
+  })
+
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+}
